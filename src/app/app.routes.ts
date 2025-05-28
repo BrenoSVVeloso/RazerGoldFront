deb04e9f37d2ddb8d23622061e2aff04d89f1ef1
BrenoSVVeloso <bbreno.valadares@gmail.com>
2025-05-28 13:43:11 -0300

    creating page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,24 @@
 import { Routes } from '@angular/router';
 import { ValorantComponent } from './pages/valorant/valorant.component';
+import { VitorPadrinhoComponent } from './pages/valorant/vitor-padrinho.component';
 
 export const routes: Routes = [
   {
-    path: 'br/menu',
+    path: 'br/menu/catalog/valorant',
     component: ValorantComponent
   },
   {
+    path:'br/menu/vitor-padrinho',
+    component: VitorPadrinhoComponent
+  },
+  
+  {
     path: '',
-    redirectTo: 'br/menu/catalog/valorant',
+    redirectTo: 'br/menu',
     pathMatch: 'full'
   },
   {
     path: '**',
-    redirectTo: 'br/menu/catalog/valorant'
+    redirectTo: 'br/menu'
   }
 ];
